5f13c1af50a23854eb305ccca2dce6707be245b6
Adriel23 <adrieldev23@gmail.com>
2026-05-08 21:49:54 -0300

    deploy

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:2323/api';
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:2323/api';
 
 /**
  * Función genérica para hacer peticiones al backend.
